a3efdaed525b27dd3efed315849d92c9a0386372
Juratbekdasturchi <juratbekgithubabduraimov@gmail.com>
2024-02-16 10:24:26 +0500

    Initial committ

---
+++ README.md
@@ -0,0 +1,3 @@
+# Olchaaaa.uz
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,80 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='shadow'>
+        <h1> Assalomu alaykum </h1>
+        <div className="container-fluid py-4">
+          <img src="https://olcha.uz/_nuxt/logo-red.46354f18.png" alt="" />
+
+        </div>
+      </header>
+      <section id="products" className="py-3">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className="d-block productImg "src="https://olcha.uz/image/700x700/products/supplier/stores/1/2023-11-27/N7Ty85z4Hti1mRvCdDHP9x2XFOedatIxL8RRrTWWGd3Af3YFkzzrsDtaYMvr.jpg" alt="" />
+                <a href="#" className="groupName d-block mb-2 mt-3"> Smartfonlar </a>
+                <p className="price mb-2"> 2 290 000 sum </p>
+                <p className="priceMonth mb-2"> 222 000 sum / oyiga. </p>
+                <p className="title"> Xiaomi Redmi 10 4/128Gb Sea Blue </p>
+                <a href="#" className="btn"> Sotib olish </a>
+
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className="d-block productImg " src="https://olcha.uz/image/700x700/products/supplier/stores/1/2023-11-27/N7Ty85z4Hti1mRvCdDHP9x2XFOedatIxL8RRrTWWGd3Af3YFkzzrsDtaYMvr.jpg" alt="" />
+                <a href="#" className="groupName d-block mb-2 mt-3"> Smartfonlar </a>
+                <p className="price mb-2"> 2 290 000 sum </p>
+                <p className="priceMonth mb-2"> 222 000 sum / oyiga. </p>
+                <p className="title"> Xiaomi Redmi 10 4/128Gb Sea Blue </p>
+                <a href="#" className="btn"> Sotib olish </a>
+
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className="d-block productImg " src="https://olcha.uz/image/700x700/products/supplier/stores/1/2023-11-27/N7Ty85z4Hti1mRvCdDHP9x2XFOedatIxL8RRrTWWGd3Af3YFkzzrsDtaYMvr.jpg" alt="" />
+                <a href="#" className="groupName d-block mb-2 mt-3"> Smartfonlar </a>
+                <p className="price mb-2"> 2 290 000 sum </p>
+                <p className="priceMonth mb-2"> 222 000 sum / oyiga. </p>
+                <p className="title"> Xiaomi Redmi 10 4/128Gb Sea Blue </p>
+                <a href="#" className="btn"> Sotib olish </a>
+
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className="d-block productImg " src="https://olcha.uz/image/700x700/products/supplier/stores/1/2023-11-27/N7Ty85z4Hti1mRvCdDHP9x2XFOedatIxL8RRrTWWGd3Af3YFkzzrsDtaYMvr.jpg" alt="" />
+                <a href="#" className="groupName d-block mb-2 mt-3"> Smartfonlar </a>
+                <p className="price mb-2"> 2 290 000 sum </p>
+                <p className="priceMonth mb-2"> 222 000 sum / oyiga. </p>
+                <p className="title"> Xiaomi Redmi 10 4/128Gb Sea Blue </p>
+                <a href="#" className="btn"> Sotib olish </a>
+
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className="d-block productImg " src="https://olcha.uz/image/700x700/products/supplier/stores/1/2023-11-27/N7Ty85z4Hti1mRvCdDHP9x2XFOedatIxL8RRrTWWGd3Af3YFkzzrsDtaYMvr.jpg" alt="" />
+                <a href="#" className="groupName d-block mb-2 mt-3"> Smartfonlar </a>
+                <p className="price mb-2"> 2 290 000 sum </p>
+                <p className="priceMonth mb-2"> 222 000 sum / oyiga. </p>
+                <p className="title"> Xiaomi Redmi 10 4/128Gb Sea Blue </p>
+                <a href="#" className="btn"> Sotib olish </a>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
